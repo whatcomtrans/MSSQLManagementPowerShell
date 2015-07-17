0d7979cbdfe191ad038c84e0ffabec22ee986b1e
R. Josh Nylander <joshn@ridewta.com>
2015-07-17 12:40:51 -0700

    Added new cmdlet Optimize-SQLIndex

--- MSSQLManagementPowerShell.psm1
+++ MSSQLManagementPowerShell.psm1
@@ -90,4 +90,63 @@ function Get-SQLLogs {
 	}
 }
 
-Export-ModuleMember -Function "Get-SQLLogs", "Get-SQLAgentJobStatus", "Get-SQLAgentJobLogs"
+<# -------------------------------------------------------------------------------------------------------
+ Marnie Ross Jan 2, 2015
+
+ This script iterates through all the indexes in all the tables of a database and 
+ based on the level of fragmentation of an index, performs on of the following:
+          If the index fragmentation is less than 5%, it does nothing
+          If the index fragmentation is between 5% and 30%, it reorganizes it
+          If the index fragmentation is over 30%, it rebuilds it
+ -------------------------------------------------------------------------------------------------------
+#>
+
+function Optimize-SQLIndex {
+	[CmdletBinding(SupportsShouldProcess=$false,DefaultParameterSetName="example")]
+	Param(
+		[Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true,ParameterSetName="example",HelpMessage="The Server instance to check.")]
+		[string] $ServerInstance,
+        [Parameter(Mandatory=$true,Position=1,ValueFromPipeline=$false,ParameterSetName="example",HelpMessage="The database to check")]
+        [string] $Database
+	)
+	Begin {
+        #Put beginning stuff here
+	}
+	Process {
+        [Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
+        $server = New-Object Microsoft.SqlServer.Management.Smo.Server $ServerInstance
+        $targetDB = $server.Databases[$Database]
+
+        foreach ($table in $targetDB.Tables)
+        {
+            foreach($index in $table.Indexes)
+            {
+                    $fragmentation = $index.EnumFragmentation()
+                    $averageFragmentation = $fragmentation.Rows[0].AverageFragmentation
+
+                    if ($averageFragmentation -lt .05)
+                    {
+                            Write-Verbose -Message "No fragmentation...$table"  -Verbose
+                            continue
+                    }
+
+                    if($averageFragmentation -ge .05 -and $averageFragmentation -lt .3)
+                    {
+                            Write-Verbose -Message "Reorganization...$table" -Verbose
+                            $index.Reorganize()
+                            continue
+                    }
+                     Write-Verbose -Message "Rebuild...$table" -Verbose
+                     if ($PSCmdlet.ShouldProcess("Rebuild...$table")) {
+                        $index.Rebuild()
+                     }
+            }
+        }
+	}
+	End {
+        #Put end here
+	}
+}
+
+
+Export-ModuleMember -Function "Get-SQLLogs", "Get-SQLAgentJobStatus", "Get-SQLAgentJobLogs", "Optimize-SQLIndex"
